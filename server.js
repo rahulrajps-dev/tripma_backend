f783c2d18b9025c6ebe49c75b1ef540e009b3522
Rahul <rahulrajps007c@gmail.com>
2026-05-11 12:00:17 +0530

    fixed render startup issue

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.get('/',(req,res)=>{
     res.send("Api is running");
 })
 
-await loadPlaceEmbeddings();
 
 mongoose.connect(process.env.MONGO_URI)
   .then(async() => {
@@ -49,4 +48,9 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
+
+  
+await loadPlaceEmbeddings();
+
+  console.log("Embeddings loaded");
 });
